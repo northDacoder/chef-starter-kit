ee392fddb5cdd8c08543ffd065e7e32002c3a68d
John Ryan <northdacoder@gmail.com>
2015-01-27 13:52:19 -0600

    added cookbook file resource from index.html

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -14,9 +14,16 @@ end
 
 
 # defined the service resource to enable & start the apache service 
-service 'httpd' do
+service "httpd" do
 	action [:enable, :start]
 end
+
+
+# defined the file resource to create a cookbook_file from the 'index.html' source
+cookbook_file "/var/www/html/index.html" do
+	source "index.html"
+	mode 0644
+end 
 
 
 
