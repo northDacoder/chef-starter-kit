449f5fdd8af039632412bc3b208ab2379d023a7a
John Ryan <northdacoder@gmail.com>
2015-01-28 11:14:02 -0600

    set apache sites attritributes & created function to dynamically create the resources for each apache site defined

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,1 +1,4 @@
-default["apache"]["indexfile"] = "index1.html"
+# default["apache"]["indexfile"] = "index1.html"
+default["apache"]["sites"]["clowns"] = { "port" => 80 }
+default["apache"]["sites"]["bears"] = { "port" => 81 }
+
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,16 +15,43 @@ end
 
 # defined the service resource to enable & start the apache service 
 service "httpd" do
-	action [:enable, :start]
+	action [ :enable, :start ]
 end
 
-node.default["apache"]["indexfile"] = "index2.html"
 
-# defined the file resource to create a cookbook_file from the 'index.html' source
-cookbook_file "/var/www/html/index.html" do
-	source node["apache"]["indexfile"]
-	mode "0644"
-end 
+execute "mv /etc/httpd/conf.d/welcome.conf
+/etc/httpd/conf.d/welcome.conf.disabled" do 
+	only_if do 
+		File.exist?("/etc/httpd/conf.d/welcome.conf")
+	end
+	notifies :restart, "service[httpd]"
+end
+
+
+node["apache"]["sites"].each do |site_name, site_data| 
+	document_root = "/srv/apache/#{site_name}" 
+
+	template "/etc/httpd/conf.d/#{site_name}.conf" do 
+		source "custom.erb"
+		mode "0644"
+		variables(:document_root => document_root,:port => site_data["port"])
+		notifies :restart, "service[httpd]"
+	end
+
+	directory document_root do
+		mode "0755"
+		recursive true
+	end
+
+	template "#{document_root}/index.html" do
+		source "index.html.erb"
+		mode "0644"
+		variables(:site_name => site_name, :port => site_data["port"])
+	end
+	
+end
+	
+	
 
 
 
